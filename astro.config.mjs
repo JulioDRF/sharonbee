df83092480d37ceaeadd1a2b81a4de25501891c7
Julio Rangel <juliod.rangelfarell@gmail.com>
2024-12-22 12:30:27 -0500

    Load template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from "@astrojs/sitemap";
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // Add your domain here
+  site: 'https://sharonbee.com',
+  integrations: [svelte(), sitemap()]
 });
